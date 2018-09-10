74760395149ebefafb7e1e0bed5434b6389ae18f
Austin  Burke <aburk3@gmail.com>
2018-09-10 00:17:52 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  true == (board[index] != " " && board[index] != "")
+  if true == (board[index] != " " && board[index] != "")
+    elsif board[index] != nil 
+  end
 end
